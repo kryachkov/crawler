6d162d009cb90102236d9b03c052d9905babdba4
Andrey Kryachkov <kryachkov.andrey@gmail.com>
2014-04-02 19:22:45 +0400

    [+] Added Page class

---
+++ spec/lib/page_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+require 'page'
+require 'rest_client'
+
+describe Page do
+  let(:googlecom_source) { File.open(File.expand_path('../../support/google.com.html', __FILE__)).read }
+
+  describe "#initialize" do
+    it "should assign url" do
+      expect(Page.new('http://google.com', 0).url).to eq('http://google.com')
+    end
+
+    it "should assign depth" do
+      expect(Page.new('http://google.com', 0).depth).to eq(0)
+    end
+  end
+
+  describe "#fetch" do
+    let(:page) { Page.new('http://google.com', 0) }
+
+    before { allow(RestClient).to receive(:get) { googlecom_source } }
+
+    it "should fetch url with RestClient" do
+      expect(RestClient).to receive(:get).with('http://google.com') { googlecom_source }
+      page.fetch
+    end
+
+    it "should parse fetched sourse with Nokogiri" do
+      expect(Nokogiri).to receive(:HTML).with(googlecom_source)
+      page.fetch
+    end
+
+    it "should assign source_code" do
+      parsed_html = double
+      allow(Nokogiri).to receive(:HTML) { parsed_html }
+      page.fetch
+      expect(page.source_code).to eq(parsed_html)
+    end
+
+    it "should return self" do
+      expect(page.fetch).to eq(page)
+    end
+  end
+
+  describe "#child_urls" do
+    let(:page) { Page.new('http://google.com', 0) }
+
+    before do
+      allow(RestClient).to receive(:get) { googlecom_source }
+      page.fetch
+    end
+
+    it "should return array of urls page refers to w/o self" do
+      expect(page.child_urls).to eq(%w(http://google.com/maps /drive))
+    end
+  end
+
+  describe "#info" do
+    let(:page) { Page.new('http://google.com', 0) }
+
+    before do
+      allow(RestClient).to receive(:get) { googlecom_source }
+      page.fetch
+    end
+
+    it "should return hash of info" do
+      expect(page.info).to eq({
+        :url => 'http://google.com',
+        :depth => 0,
+        :child_urls => %w(http://google.com/maps /drive),
+        :input_count => 2
+      })
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'rspec'
+require 'simplecov'
+
+SimpleCov.start
+
+$LOAD_PATH.unshift(File.expand_path(File.join(__FILE__, '../..')))
